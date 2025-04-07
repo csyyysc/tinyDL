e0fcb1b4a331644950084e9b9bb131cde053766b
csyyysc <matuyou0301@gmail.com>
2025-04-07 13:19:52 +0900

    style: 💄 format codes

--- bindings.cpp
+++ bindings.cpp
@@ -6,8 +6,7 @@ namespace py = pybind11;
 // 外部聲明 CUDA 函數
 extern "C" void matrixMultiply(float *A, float *B, float *C, int M, int N, int K);
 
-void matrixMultiplyWrapper(
-    py::array_t<float> A, py::array_t<float> B, py::array_t<float> C, int M, int N, int K) {
+void matrixMultiplyWrapper(py::array_t<float> A, py::array_t<float> B, py::array_t<float> C, int M, int N, int K) {
     auto buf_A = A.request();
     auto buf_B = B.request();
     auto buf_C = C.request();
