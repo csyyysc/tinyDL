d9e1e57c67522ae962285bc7683e620285262517
csyyysc <matuyou0301@gmail.com>
2025-04-07 23:11:13 +0900

    feat: 🎸 ReLU kernel added
    Will be used in MLP.

---
+++ module/ActivationKernels.cuh
@@ -0,0 +1,8 @@
+#ifndef ACTIVATION_KERNELS_H
+#define ACTIVATION_KERNELS_H
+
+__global__ void reluKernel(float *input, float *output, int size);
+
+__global__ void reluBackwardKernel(float *input, float *grad_output, float *grad_input, int size);
+
+#endif
